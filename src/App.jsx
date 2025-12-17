26edaef94ecd86433ce484d12414f441726920c6
Aaradhy <aaradhyartist@gmail.com>
2025-12-17 23:37:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,20 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { ROUTES } from "./constants/routes";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 import MainLayout from "./layouts/MainLayout";
-import Contact from "./pages/Contact";
-import PrivateRoute from "./components/PrivateRoute";
-import Login from "./pages/Login";
+
 import { useDispatch, useSelector } from "react-redux";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import SignUp from "./pages/SignUp";
+
 import { fetchUserProfile } from "./services/authService";
 import { setUser } from "./store/authSlice";
 import { useEffect } from "react";
-import ApiTester from "./pages/ApiTester";
+
 import ContactUs from "./pages/Contact";
 import WhatsAppContact from "./pages/Whatsapp";
-import BackToTop from "./components/Home/BackToTop.JSX";
+
 import Navbar from "./components/Home/Navbar";
 import Footer from "./components/Home/Footer";
 import Service from "./pages/Service";
